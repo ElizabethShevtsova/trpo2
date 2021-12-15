18cc8ff66d1a7a59a9ca3b3cd4f033391781bbb8
Elizaveta <fox432shevtsova@yandex.ru>
2021-12-15 18:31:31 +0300

    lab4.1

---
+++ ShevtsovaLab1/core/LogInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace core
+{
+   public interface LogInterface
+   {
+         LogInterface log(string str);
+         LogInterface write();
+
+    }
+}
